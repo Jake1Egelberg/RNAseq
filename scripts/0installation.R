8590bef2fc66e984b9ca0a603101dbb1950098e0
Jake Egelberg <jake1egelberg@gmail.com>
2021-08-15 19:12:02 -0400

    Add files via upload

---
+++ scripts/0installation.R
@@ -0,0 +1,13 @@
+install.packages(c("BiocManager",
+                   "stringr",
+                   "RColorBrewer",
+                   "gplots"))
+
+BiocManager::install(c("Rsubread", 
+                       "edgeR",
+                       "limma",
+                       "Glimma",
+                       "org.Mm.eg.db",
+                       "org.Hs.eg.db",
+                       "fgsea",
+                       "reactome.db"))
